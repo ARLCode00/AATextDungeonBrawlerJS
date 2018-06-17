3efd652510434a8d77c47093d8b5cb4b7b5f41f0
ARLCode00 <alfredo.rl00@gmail.com>
2018-06-17 14:01:22 -0400

    Small refactor

--- loader.js
+++ loader.js
@@ -1,9 +1,11 @@
-const Model = require('./model.js');
+const E = require('./environment.js');
+const C = require('./characters.js');
 
-var room = new Model.Room();
+
+var room = new E.Room();
 room.name = 'Entrance';
 
-var foe = new Model.Enemy('Vampire', 100, 75, 50, 200, 15, 'vamp');
+var foe = new C.Enemy('Vampire', 100, 75, 50, 200, 15, 'vamp');
 
 console.log(room.name);
 console.log(foe.getTitle(true));
